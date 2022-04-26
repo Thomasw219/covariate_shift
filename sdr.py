23607b9d03464c9ed44ffc709b8c497d23e0ac16
Thomas Wei <thomasw219@gmail.com>
2022-04-25 21:50:09 -0400

    got kernal mean matching from other repo, cite later, starting experiments

--- sdr.py
+++ sdr.py
@@ -1,6 +1,8 @@
 import torch
+import numpy as np
 
 from toy_data import generate_toy_data
+from kmmClassification import kmm
 
 def gaussian_kernel_gram_matrix(X, sigma=0.1, l=1):
     n = X.shape[1]
@@ -14,7 +16,6 @@ def centralized_gram_matrix(X):
     d = X.shape[0]
     gram_matrix = gaussian_kernel_gram_matrix(X)
     center = torch.eye(n, device=X.device) - 1 / n * torch.ones(n, n)
-#    print(gram_matrix)
     return center @ gram_matrix @ center
 
 def conditional_covariance_matrix(K_cy, K_cy2, K_cu, epsilon=0.01):
@@ -22,28 +23,52 @@ def conditional_covariance_matrix(K_cy, K_cy2, K_cu, epsilon=0.01):
     identity = torch.eye(n, device=K_cy.device)
     inner = torch.inverse(K_cu + epsilon * identity)
     outer = K_cy @ K_cu
-#    print(torch.trace(K_cy2), torch.trace(outer @ inner @ inner @ outer.T))
     return K_cy2 - outer @ inner @ inner @ outer.T
 
 if __name__ == "__main__":
-    n = 100
-    d = 50
-    X_tr, y_tr, X_te, y_te = generate_toy_data(5, n, n)
-    K_cy = centralized_gram_matrix(y_tr)
-    K_cx = centralized_gram_matrix(X_tr)
-    identity = torch.eye(n, device=K_cy.device)
-    K_cy2 = (K_cy + 0.01 * identity) @ (K_cy + 0.01 * identity)
-    objective_no_reduction = torch.trace(conditional_covariance_matrix(K_cy, K_cy2, K_cx))
-    print(objective_no_reduction)
+    n_samples = 10
+    all_data = []
+    device = torch.device("cuda:0")
+    for sample in range(n_samples):
+        data = []
+        for d in range(1, 21):
+            n = 500
+            X_tr, y_tr, X_te, y_te, betas_true = generate_toy_data(d, n, n, device=device)
+            K_cy = centralized_gram_matrix(y_tr)
+            identity = torch.eye(n, device=K_cy.device)
+            K_cy2 = (K_cy + 0.01 * identity) @ (K_cy + 0.01 * identity)
 
-    K_cu = centralized_gram_matrix(X_tr[1:, :])
-    objective_wrong_reduction = torch.trace(conditional_covariance_matrix(K_cy, K_cy2, K_cu))
-    print(objective_wrong_reduction)
+            d_r = 1
+            iterations = 500
+            eta = 0.5
+            W = torch.ones(d, d_r)
+            torch.nn.init.orthogonal_(W)
+            for i in range(iterations):
+                W = W / torch.norm(W.flatten())
+                check = W.T @ W
+                check[check < 1e-3] = 0
+                W = W.detach()
+                W.requires_grad = True
 
-    K_cu = centralized_gram_matrix(X_tr[0:1, :])
-    objective_full_reduction = torch.trace(conditional_covariance_matrix(K_cy, K_cy2, K_cu))
-    print(objective_full_reduction)
+                U_tr = W.T @ X_tr
+                K_cu = centralized_gram_matrix(U_tr)
+                objective = torch.trace(conditional_covariance_matrix(K_cy, K_cy2, K_cu))
+                print(objective.item())
 
-    print(objective_full_reduction < objective_wrong_reduction)
-    print(objective_no_reduction < objective_wrong_reduction)
-    print(objective_full_reduction < objective_no_reduction)
+                print(W.T)
+                W.grad = None
+                objective.backward()
+                W = W.detach() - eta * W.grad
+                if np.abs(np.abs(W[0, 0]) - 1) < 0.025:
+                    break
+
+            W = W / torch.norm(W.flatten())
+            U_tr = W.T @ X_tr
+            U_te = W.T @ X_te
+
+            betas = torch.tensor(kmm(U_tr.cpu().numpy().T.astype(np.double), U_te.cpu().numpy().T.astype(np.double), 0.01))
+            data.append(torch.sum(torch.square(betas_true - betas)).item())
+        print(data)
+        all_data.append(data)
+    print(torch.tensor(all_data))
+    torch.save(all_data, "low_dim_errors.pt")
--- toy_data.py
+++ toy_data.py
@@ -1,10 +1,16 @@
 import torch
+from torch.distributions.normal import Normal
+from torch.distributions.independent import Independent
 
 def generate_toy_data(dim, n_tr, n_te, device=torch.device("cpu")):
     X_tr = torch.normal(torch.zeros(dim, n_tr, device=device), torch.ones(dim, n_tr, device=device))
     y_tr = torch.sigmoid(X_tr[0:1, :] + 5 * torch.tan(X_tr[0:1, :]))
+    X_tr_dist = Independent(Normal(torch.tensor([0]), torch.tensor([1])), 1)
 
     X_te = torch.normal(0.5 * torch.ones(dim, n_te, device=device), torch.ones(dim, n_te, device=device))
     y_te = torch.sigmoid(X_te[0:1, :] + 5 * torch.tan(X_te[0:1, :]))
+    X_te_dist = Independent(Normal(torch.tensor([0.5]), torch.tensor([1])), 1)
 
-    return X_tr, y_tr, X_te, y_te
+    beta = torch.exp(X_te_dist.log_prob(X_tr.T)) / torch.exp(X_tr_dist.log_prob(X_tr.T))
+
+    return X_tr, y_tr, X_te, y_te, beta
